45416b952a1725fbba6bfe3c91a5d7956b8a38ee
saisumanthkacham <63229560+saisumanthkacham@users.noreply.github.com>
2021-07-22 01:07:47 +0530

    style: refactor

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom"
 
 export const SideBar=({setSideBarDisplay})=>{
    
-    return <>
+    return <div className="side-bar-wrapper">
 
     <div className="side-bar center-col">
     <div className="burger-btn" onClick={()=>{setSideBarDisplay(false)}}><i className="fas fa-times icon-sm saved-cd-wrong fa-1x " ></i></div> 
@@ -16,7 +16,8 @@ export const SideBar=({setSideBarDisplay})=>{
     <NavLink to="/saved"  activeClassName="active-btn bold" className="btn btn-lg">Saved</NavLink>
     <NavLink to="/liked"  activeClassName="active-btn bold" className="btn btn-lg">Liked</NavLink>
     <NavLink to="/about"  activeClassName="active-btn bold" className="btn btn-lg">About</NavLink>
+    <hr/>
     </div>
-    
-    </>
+   
+    </div>
 }
